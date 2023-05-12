be2e0c32d4f9597fc1ba9effd9a63771f41eff32
Daniel McGill <danm364@yahoo.com>
2023-05-12 00:12:20 -0400

    make styling updates try to make responsive to tablet and desktop

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,8 +5,11 @@ import searchSvg from '../assets/images/icons/search.svg'
 function SearchBar() {
     return (
       <div className="header__search-wrap">
-        <input type="text" placeholder="Search" className='header__search-bar'/>
-        <object data={searchSvg} className='header__searchSvg'></object>
+        <div className='header__anchor'>
+          <input type="text" placeholder="Search" className='header__search-bar'/>
+          <object data={searchSvg} className='header__searchSvg'></object>
+        </div>
+        
         <img src={userImg} alt="user-img" className='header__user-img'/>
       </div>
     );
--- src/components/UploadBtn.js
+++ src/components/UploadBtn.js
@@ -1,11 +1,12 @@
-import userImg from '../assets/images/Mohan-muruge.jpg'
 
+import uploadSvg from '../assets/images/icons/upload.svg'
 
 
 function UploadBtn() {
     return (
       <div className="header__upload-wrap">
         <button className='header__upload-btn'>UPLOAD</button>
+        <object data={uploadSvg} className='header__uploadSvg' aria-label="upload Svg"></object>
       </div>
     );
   }
