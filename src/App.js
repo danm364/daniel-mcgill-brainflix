56e6f37ec1186dc0fa0d56d3b49613160cbef33e
Daniel McGill <danm364@yahoo.com>
2023-06-01 07:36:46 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Header from "./components/header/Header";
 import Body from "./components/body/Body";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Body />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <Header />
+          <Body />
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
