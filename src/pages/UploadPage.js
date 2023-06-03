96c1385e8d77e3270b9f346609feda32041ca0d7
Daniel McGill <danm364@yahoo.com>
2023-06-03 17:24:37 -0400

    updat eheader to make less complex, also started uploadpage

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,3 +1,5 @@
+import publishSvg from "../assets/images/icons/publish.svg"; 
+
 
 
 function UploadPage() {
@@ -6,7 +8,26 @@ function UploadPage() {
     
     return (
         <div className="uploadpage__container"> 
-            <h1>hello world</h1>
+            <h1>Upload Video</h1>
+            <section className="uploadpage__video-info">
+                <div className="uploadpage__image-container">
+                    <label className="uploadpage__label" htmlFor="">VIDEO THUMBNAIL</label>
+                    <img src="" alt=""  />
+                </div>
+                <div className="uploadpage__info-container">
+                    <label className="uploadpage__label" htmlFor="">TITLE YOUR VIDEO</label>
+                    <input type="text" placeholder="Add a title to your video"/>
+                </div>
+                <div className="uploadpage__info-container"> 
+                    <label className="uploadpage__label" placeholder="" htmlFor="">ADD A VIDEO DESCRIPTION</label>
+                    <input type="text" placeholder="Add a description to your video" />
+                </div>
+            </section>
+            <div className="uploadpage__btn-container">
+                <button className="uploadpage__publish-btn">PUBLISH</button>
+                <object data={publishSvg} aria-label="publishSvg" className="uploadpage__publishSvg" type=""></object>
+            </div>
+            <p>CANCEL</p>
         </div>
         
     );
