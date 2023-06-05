fc8a824b3dbbac381effc21cc34a35855d933cd5
Daniel McGill <danm364@yahoo.com>
2023-06-05 08:18:12 -0400

    make updates to upload page

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,5 +1,5 @@
 import publishSvg from "../assets/images/icons/publish.svg"; 
-
+import bikeImage from "../assets/images/Upload-video-preview.jpg";
 
 
 function UploadPage() {
@@ -7,12 +7,13 @@ function UploadPage() {
 
     
     return (
+        
         <div className="uploadpage__container"> 
             <h1>Upload Video</h1>
             <section className="uploadpage__video-info">
                 <div className="uploadpage__image-container">
                     <label className="uploadpage__label" htmlFor="">VIDEO THUMBNAIL</label>
-                    <img src="" alt=""  />
+                    <img src={bikeImage} alt=""  />
                 </div>
                 <div className="uploadpage__info-container">
                     <label className="uploadpage__label" htmlFor="">TITLE YOUR VIDEO</label>
@@ -20,14 +21,17 @@ function UploadPage() {
                 </div>
                 <div className="uploadpage__info-container"> 
                     <label className="uploadpage__label" placeholder="" htmlFor="">ADD A VIDEO DESCRIPTION</label>
-                    <input type="text" placeholder="Add a description to your video" />
+                    <textarea type="text" className="uploadpage__info-desc" placeholder="Add a description to your video" />
                 </div>
             </section>
             <div className="uploadpage__btn-container">
-                <button className="uploadpage__publish-btn">PUBLISH</button>
-                <object data={publishSvg} aria-label="publishSvg" className="uploadpage__publishSvg" type=""></object>
+                <div className="uploadpage__anchor ">
+                    <button className="uploadpage__publish-btn">PUBLISH</button>
+                    <object data={publishSvg} aria-label="publishSvg" className="uploadpage__publishSvg" type=""></object>
+                    <button className="uploadpage__cancel-btn">CANCEL</button>
+                </div>
             </div>
-            <p>CANCEL</p>
+            
         </div>
         
     );
